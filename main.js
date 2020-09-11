2a6d1361a6900dd4a9ff1d0a4c6015724d9e9d01
RLAVAN-RAGHURAM <70435014+RLAVAN-RAGHURAM@users.noreply.github.com>
2020-09-11 16:47:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ if(status != "")
 {
 for (i=0;i<objects.length;i++)
 {
-document.getElementById("status").innerHTML="status : Object detected"
+document.getElementById("status").innerHTML="status : Object detected";
 fill("#FF0000");
 percentage=floor(objects[i].confidence*100);
 text(objects[i].label+ " " +percentage+"%",objects[i].x+15,objects[i].y+15);
